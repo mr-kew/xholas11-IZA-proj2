6a4350107cb2ea6308f1a80671ee97d7e3582df1
David Holas <mrkew48@gmail.com>
2020-05-13 01:32:34 +0200

    Project init

---
+++ proj2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proj2
+//
+//  Created by David on 13/05/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
